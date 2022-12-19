7f5049a0e19c7100b584c34177291a24fec9807f
abhilashlegend <abhilash456a@live.com>
2022-12-19 22:10:46 +0530

    String Reverse Program

---
+++ ReverseString/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Please enter a string");
+Console.ForegroundColor = ConsoleColor.Yellow;
+string input = Console.ReadLine().ToString();
+
+string reverse = "";
+
+for(int i = input.Length - 1; i >= 0; i--)
+{
+    reverse += input[i];
+}
+
+Console.WriteLine(reverse);
+Console.ReadLine();

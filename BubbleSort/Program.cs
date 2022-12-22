030eb486b3bad7e791b5b59a0ebad26c07895786
abhilashlegend <abhilash456a@live.com>
2022-12-22 19:41:04 +0530

    Bubble sort Program

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] arr = { 85, 45, 58, 12, 28, 2, 77, 99, 105, 65 };
+
+for(int i = 0; i < arr.Length; i++)
+{
+    for(int j = 0; j < arr.Length; j++)
+    {
+        if (arr[i] < arr[j])
+        {
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+for(int k = 0; k < arr.Length; k++)
+{
+    Console.WriteLine(arr[k]);
+}
+Console.ReadLine();
+

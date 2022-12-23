a0fc4fee9e74b55694ff2f1af1b6484aba790e44
abhilashlegend <abhilash456a@live.com>
2022-12-23 15:29:52 +0530

    Odd or Even Program

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Please enter a number");
+string num = Console.ReadLine();
+int val;
+bool isNum = int.TryParse(num, out val);
+
+if (isNum)
+{
+    if(val % 2 == 0)
+    {
+        Console.WriteLine("Entered number is even");
+    }
+    else
+    {
+        Console.WriteLine("Entered number is odd");
+    }
+} 
+else
+{
+    Console.WriteLine("You have not entered number, try again.");
+}

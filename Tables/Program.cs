4431d6ce425f27868747c3c2728665d7a83370c6
abhilashlegend <abhilash456a@live.com>
2022-12-23 11:20:48 +0530

    Table Program

---
+++ Tables/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Enter the number to print table");
+try
+{
+    int num = int.Parse(Console.ReadLine());
+    if (num != null)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{num} x {i} = {num * i}");
+        }
+    }
+} catch(System.FormatException)
+{
+    Console.WriteLine("Please enter a number next time");
+}
+
+Console.ReadLine();

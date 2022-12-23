d561055d91378fe8fcfbe15af8b695e36a3543b7
abhilashlegend <abhilash456a@live.com>
2022-12-23 15:10:09 +0530

    TryParse example

---
+++ TryParse/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Please enter your age");
+int age;
+bool flag =   int.TryParse(Console.ReadLine(), out age);
+
+/* If user entered number */
+if (flag)
+{
+    if(age > 18)
+    {
+        Console.WriteLine("You are eligible for voting");
+    } else
+    {
+        Console.WriteLine("You are not eligible for voting");
+    }
+
+} 
+else
+{
+    Console.WriteLine("Invalid entry, please try again");
+}

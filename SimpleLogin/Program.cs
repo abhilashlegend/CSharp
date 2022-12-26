4ff0d8f1d8d5549f9ed864b744692a0d8a6b1991
abhilashlegend <abhilash456a@live.com>
2022-12-26 20:34:16 +0530

    Simple Login

---
+++ SimpleLogin/Program.cs
@@ -0,0 +1,34 @@
+﻿string username;
+string password;
+
+void Register()
+{
+    Console.WriteLine("Please enter your username");
+    username = Console.ReadLine();
+    Console.WriteLine("Please enter your password");
+    password = Console.ReadLine();
+    Console.WriteLine("Registeration completed. Thank you.");
+    Console.WriteLine("---------------------------------------------");
+}
+
+void Login()
+{
+    Console.WriteLine("Please enter your username");
+    string uname = Console.ReadLine();
+    Console.WriteLine("Please enter your password");
+    string pwd = Console.ReadLine();
+    if(username == uname && password == pwd)
+    {
+        Console.WriteLine("Login Successfull");
+    } 
+    else
+    {
+        Console.WriteLine("Invalid Login details, please try again");
+    }
+}
+
+Register();
+Login();
+Console.ReadLine();
+
+

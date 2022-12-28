21708fd90c88290023fbf6abd290c7df6f69265f
abhilashlegend <abhilash456a@live.com>
2022-12-28 19:39:18 +0530

    Ternary operator example Program

---
+++ Ternary/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter temperature");
+string inpTemp = Console.ReadLine();
+int temp;
+bool isNum = int.TryParse(inpTemp, out temp);
+string status;
+
+if (isNum)
+{
+    status = temp < 15 ? "it\'s to cold here" : temp >= 16 && temp <= 28 ? "it is ok" : "It is hot here";
+    Console.WriteLine(status);
+} else
+{
+    Console.WriteLine("Not a valid temperature");
+}
+
+Console.ReadLine();

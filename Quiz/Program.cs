4ef9745b011e52213b1c50830cb4dfa4ee71bbcd
abhilashlegend <abhilash456a@live.com>
2022-12-28 19:13:57 +0530

    added simple quiz program

---
+++ Quiz/Program.cs
@@ -0,0 +1,35 @@
+﻿class Quiz
+{
+    string[] question = {"Which is the capital city of India", "Where is Taj Mahal", "Which is the IT Hub of India" };
+
+    string[] ans = {"Delhi", "Agra", "Bangalore" };
+
+    int score = 0;
+
+    public void play()
+    {
+        for(int i = 0; i < question.Length; i++)
+        {
+            Console.WriteLine(question[i]);
+            string answer = Console.ReadLine();
+            if(answer == ans[i])
+            {
+                score++;
+            }
+        }
+        Console.WriteLine("Your score is " + score);
+    }
+
+    public static void Main(string[] args)
+    {
+        Quiz quiz = new Quiz();
+        quiz.play();
+    }
+
+}
+
+
+
+
+
+

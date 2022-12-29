d8aa246788809f90a30b1c163a84d1a1a5ecd92a
abhilashlegend <abhilash456a@live.com>
2022-12-29 20:10:31 +0530

    added loops program

---
+++ Loops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static void ForLoop()
+        {
+            // TODO
+            for(int i = -3; i <= 3; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void WhileLoop()
+        {
+            int i = 3;
+            while(i >= -3)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+            WhileLoop();
+            ForLoop();
+            Console.ReadLine();
+        }
+
+    }
+}

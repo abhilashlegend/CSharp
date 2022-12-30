d46389a08021caab44a9c0b795b3453d0383a5fe
abhilashlegend <abhilash456a@live.com>
2022-12-30 17:28:43 +0530

    class demo

---
+++ Class/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Person
+{
+    public string firstName;
+    public string lastName;
+
+    public void introduce()
+    {
+        Console.WriteLine($"Hi, I am {firstName} {lastName}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Person ab = new Person();
+        ab.firstName = "Abhilash";
+        ab.lastName = "N";
+        ab.introduce();
+
+        Person raj = new Person();
+        raj.firstName = "RajKishor";
+        raj.lastName = "Kattimani";
+        raj.introduce();
+
+        Console.ReadLine();
+
+    }
+}
---
+++ ContinueBreak/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static void Main(string[] args)
+        {
+            int i = -10;
+
+            while (true)
+            {
+                if(i % 3 == 0)
+                {
+                    i++;
+                    continue;
+                }
+
+                if(i == 10) break;
+                if (i % 6 == 0)
+                {
+                    break;
+                }
+                Console.WriteLine(i++);
+            }
+        }
+    }
+}

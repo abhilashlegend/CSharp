c4c016c8bbc0dc28bb210fac6bdc55768f1f8f0b
abhilashlegend <abhilash456a@live.com>
2022-12-30 20:21:27 +0530

    Constructor example

---
+++ Constructor/Program.cs
@@ -0,0 +1,34 @@
+﻿class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+    private string eyeColor;
+
+    public Person(string firstName, string lastName, int age, string eyeColor)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;   
+        this.age = age;
+        this.eyeColor = eyeColor;
+    }
+
+    public void introduce()
+    {
+        Console.WriteLine($"Hi, this is {firstName} {lastName} and I'm {age} years old. My eye color is {eyeColor}");
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Person ab = new Person("Abhilash", "N", 34, "brown");
+        ab.introduce();
+
+        Person rj = new Person("Rajkishor", "kattimani", 34, "brown");
+        rj.introduce();
+
+        Console.ReadLine();
+    }
+}

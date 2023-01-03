bce9b62f1249350623bcf438a13e2b840b7e45b4
abhilashlegend <abhilash456a@live.com>
2023-01-03 22:10:46 +0530

    Demo of array, for and foreach

---
+++ Marks/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] subjects = new string[] { "English", "Kannada", "Hindi", "Maths", "Science", "Social" };
+
+int[] marks = new int[6];
+
+Console.WriteLine("Enter the Marks");
+for(int i = 0; i < subjects.Length; i++)
+{
+    Console.WriteLine("Enter marks for {0}", subjects[i]);
+    marks[i] = int.Parse(Console.ReadLine());
+}
+
+int sum = 0;
+
+foreach(int mark in marks)
+{
+    sum += mark;
+}
+Console.WriteLine("Total Marks = {0}", sum);
+Console.ReadLine();

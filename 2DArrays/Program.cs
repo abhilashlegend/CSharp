0d8fde61ea1b8a0ba7300fe553ed3b8e180049f9
abhilashlegend <abhilash456a@live.com>
2023-01-05 17:15:15 +0530

    2d array example

---
+++ 2DArrays/Program.cs
@@ -0,0 +1,53 @@
+﻿int[,] matrix =
+{
+   {1,2,3},
+   {4,5,6},
+   {7, 8,9}
+};
+
+Console.WriteLine("The elements in the matrix are as follows:");
+
+foreach(int item in matrix)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("-------------------------------------------------");
+
+Console.WriteLine("Using nested for");
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = matrix[i, j] * 2;
+        Console.WriteLine(matrix[i, j]);
+    }
+}
+
+Console.WriteLine("---------------------------------------------------");
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if(i == j)
+        Console.WriteLine(matrix[i, j]);
+    }
+}
+
+Console.WriteLine("---------------------------------------------------");
+
+int inc = 0;
+int dec = 2;    
+
+while(inc < matrix.GetLength(0))
+{
+    
+        Console.WriteLine(matrix[inc, dec]);
+        inc++;
+        dec--;
+
+}
+
+Console.ReadLine();

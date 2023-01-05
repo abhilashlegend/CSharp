8d0d81b648951263c6e03431f291f2f0c244558d
abhilashlegend <abhilash456a@live.com>
2023-01-05 12:57:48 +0530

    MultiDimensional Arrays

---
+++ MultiDimensionalArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿int[,] matrix = new int[,]
+{
+    {1, 2, 3 },
+    {4, 5, 6 },
+    {7, 8, 9 }
+};
+
+Console.WriteLine("This is an example of 2D array {0}", matrix[1, 1]);
+Console.WriteLine("The rank of the array is {0}", matrix.Rank);
+
+
+string[,,] threeD = {
+    {
+        {
+            "Abhilash", "Akhil", "Abhishek"
+        },
+        {
+            "Bhasavraj", "Basu", "Banu"
+        },
+        {
+            "Chetan", "Chaitra", "Chandani"
+        } 
+    }
+};
+
+Console.WriteLine("This is an example of 3 D Array. Lets pick Banu, {0}", threeD[0, 1, 2]);
+
+
+Console.ReadLine();

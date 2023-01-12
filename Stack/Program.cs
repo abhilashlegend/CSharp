5d5089c006a3c514d7085063513f6362c17c9afe
abhilashlegend <abhilash456a@live.com>
2023-01-12 16:43:08 +0530

    stack

---
+++ Stack/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+Stack<int> nums = new Stack<int>();
+
+for(int i = 0; i < 5; i++)
+{
+    nums.Push(i);
+}
+
+
+Console.WriteLine(nums.Peek());
+
+if(nums.Count > 0)
+{
+    nums.Pop();
+}
+
+Console.WriteLine(nums.Peek());
+

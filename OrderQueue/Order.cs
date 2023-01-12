9e723fca0bfc99907acdf852185c135311e4c797
abhilashlegend <abhilash456a@live.com>
2023-01-12 17:36:56 +0530

    an example of queue

---
+++ OrderQueue/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderQueue
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public int Qty { get; set; }
+
+        public Order(int id, int qty)
+        {
+            this.Id= id;
+            this.Qty = qty;
+        }
+
+        public void ProcessOrder()
+        {
+            Console.WriteLine($"Order {Id} processed!.");
+        }
+    }
+}
---
+++ OrderQueue/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace OrderQueue
+{
+    public class Program
+    {
+        static Order[] ReceiveOrdersFromBranch1()
+        {
+            Order[] orders = new Order[]
+             {
+                new Order(1, 2),
+                new Order(2, 3),
+                new Order(3, 4),
+             };
+            return orders;
+        }
+
+        static Order[] ReceiveOrdersFromBranch2()
+        {
+            Order[] orders = new Order[]
+             {
+                new Order(4, 5),
+                new Order(5, 4),
+                new Order(6, 10),
+             };
+            return orders;
+        }
+        public static void Main(string[] args)
+        {
+            Queue<Order> ordersQueue = new Queue<Order>();
+
+            foreach(Order o in ReceiveOrdersFromBranch1())
+            {
+                ordersQueue.Enqueue(o);
+            }
+
+            foreach (Order o in ReceiveOrdersFromBranch2())
+            {
+                ordersQueue.Enqueue(o);
+            }
+
+            while(ordersQueue.Count > 0)
+            {
+                Order currentOrder = ordersQueue.Dequeue();
+
+                currentOrder.ProcessOrder();
+
+            }
+        }
+    }
+}

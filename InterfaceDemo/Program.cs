f004fddccd595d733bf6d173da93df6650ccff43
abhilashlegend <abhilash456a@live.com>
2023-01-23 20:28:37 +0530

    Interface demo

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using InterfaceDemo;
+
+namespace IntefaceDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Ticket t1 = new Ticket(4);
+            Ticket t2 = new Ticket(4);
+
+            Console.WriteLine(t1.Equals(t2));
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ InterfaceDemo/Ticket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    public class Ticket : IEquatable<Ticket>
+    {
+        public int Duration { get; set; }
+
+        public Ticket(int duration)
+        {
+            Duration = duration;
+        }
+
+        public bool Equals(Ticket other)
+        {
+            return this.Duration == other.Duration; 
+        }
+    }
+}

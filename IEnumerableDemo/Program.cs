a94e05e5071dc8e991e2ed0aa0c080b7375e48ad
abhilashlegend <abhilash456a@live.com>
2023-01-26 21:37:26 +0530

    IEnumerable demo 2

---
+++ IEnumerableDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace IEnumerableDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> nums = new List<int>() { 23, 23, 34, 2 };
+            Console.WriteLine("The sum is {0}", voidSumCollection(nums));
+
+            int[] numarray = new int[] { 23, 22, 2 };
+            Console.WriteLine("The sum of array is {0}", voidSumCollection(numarray));
+
+            Console.ReadLine();
+
+        }
+
+        public static int voidSumCollection(IEnumerable<int> collection)
+        {
+            int sum = 0;
+
+            foreach(int num in collection)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}

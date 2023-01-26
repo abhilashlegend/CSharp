965829d083e6dd078ac96825e2ca10a83cc20a12
abhilashlegend <abhilash456a@live.com>
2023-01-26 21:22:55 +0530

    IEnumerable example

---
+++ IEnumerable/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace IEnumerable
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IEnumerable<int> unknownCollection;
+            unknownCollection = getCollection(1);
+
+            foreach(int num in unknownCollection)
+            {
+                Console.WriteLine(num + " ");
+            }
+
+            Console.WriteLine(" ");
+
+            unknownCollection = getCollection(2);
+
+            foreach(int num in unknownCollection)
+            {
+                Console.WriteLine(num + " ");
+            }
+
+            Console.WriteLine(" ");
+
+            unknownCollection = getCollection(3);
+
+            foreach(int num in unknownCollection)
+            {
+                Console.WriteLine(num + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static IEnumerable<int> getCollection(int option)
+        {
+            if(option == 1)
+            {
+                List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+                return list;
+            }
+            else if(option == 2)
+            {
+                Queue<int> que = new Queue<int>();
+                que.Enqueue(6);
+                que.Enqueue(7);
+                que.Enqueue(8);
+                que.Enqueue(9);
+                que.Enqueue(10);
+                return que;
+            } 
+            else
+            {
+                return new int[] { 11, 12, 13, 14 };
+            }
+        }
+    }
+}

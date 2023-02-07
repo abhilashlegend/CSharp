1e3b699cde28fbe91a3a5fbe932073ec70915d0c
abhilashlegend <abhilash456a@live.com>
2023-02-07 18:30:37 +0530

    As is example

--- Abstract/Program.cs
+++ Abstract/Program.cs
@@ -10,6 +10,21 @@
             {
                 shape.GetInfo();
                 Console.WriteLine("The shape is {0} and has a volume of {1}", shape.Name, shape.Volume());
+
+                Cube cb = shape as Cube;
+                if(cb == null)
+                {
+                    Console.WriteLine("This shape is no cube");
+                }
+                if(shape is Cube)
+                {
+                    Console.WriteLine("This is a cube");
+                }
+
+                object cube1 = new Cube(7);
+                Cube cube2 = (Cube)cube1;
+
+                Console.WriteLine("{0} has a Volume of {1}", cube2.Name, cube2.Volume());
             }
 
             Console.ReadLine();

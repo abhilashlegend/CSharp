cf861e59b8c68b3227b70c6c396a2a8a02155bf4
abhilashlegend <abhilash456a@live.com>
2023-02-07 16:32:30 +0530

    Abstract class

---
+++ Abstract/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Abstract
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Shape[] shapes = { new Cube(3), new Sphere(5) };
+
+            foreach(Shape shape in shapes)
+            {
+                shape.GetInfo();
+                Console.WriteLine("The shape is {0} and has a volume of {1}", shape.Name, shape.Volume());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

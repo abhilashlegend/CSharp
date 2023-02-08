b20ccfda187fdf6e9b839988460620794d156a9e
abhilashlegend <abhilash456a@live.com>
2023-02-08 18:16:43 +0530

    Reading text file

---
+++ ReadTxtFile/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ReadTxtFile
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string text = System.IO.File.ReadAllText(Directory.GetCurrentDirectory() + @"\Note.txt");
+
+            Console.WriteLine("Your file content is {0}", text);
+            
+            Console.ReadLine();
+        }
+    }
+}

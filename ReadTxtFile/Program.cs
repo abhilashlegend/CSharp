396e52dd8ad0dafb1e8f954315011c38f9519c37
abhilashlegend <abhilash456a@live.com>
2023-02-08 20:15:06 +0530

    writing to file

--- ReadTxtFile/Program.cs
+++ ReadTxtFile/Program.cs
@@ -8,6 +8,15 @@
             string text = System.IO.File.ReadAllText(Directory.GetCurrentDirectory() + @"\Note.txt");
 
             Console.WriteLine("Your file content is {0}", text);
+
+            string[] lines = System.IO.File.ReadAllLines(Directory.GetCurrentDirectory() + @"\Note.txt");
+
+            Console.WriteLine("Contents of text file =");
+
+            foreach(string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
             
             Console.ReadLine();
         }
---
+++ WriteTextFile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace WriteTextFile
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Method 1:
+            string[] lines = { "first line", "second line", "third line" };
+
+            File.WriteAllLines(Directory.GetCurrentDirectory() + @"\textFile.txt", lines);
+
+            Console.ReadLine();
+
+            // Method 2: 
+            Console.WriteLine("Please give the file a name");
+            string filename = Console.ReadLine();
+            Console.WriteLine("Please enter the text file");
+            string input = Console.ReadLine();
+            File.WriteAllText(Directory.GetCurrentDirectory() + @"\" + filename + ".txt", input);
+        }
+    }
+}

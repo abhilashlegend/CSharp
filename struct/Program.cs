90db6139ac991d765d7e2825b58af5e24a052915
abhilashlegend <abhilash456a@live.com>
2023-02-23 20:11:01 +0530

    Struct example

---
+++ struct/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace structExample {
+
+    public struct Game
+    {
+        public string name;
+        public string developer;
+        public double rating;
+        public string releasedate;
+
+        public void displayInfo()
+        {
+            Console.WriteLine("The name of the game is : {0}", name);
+            Console.WriteLine("The developer of the game is : {0}", developer);
+            Console.WriteLine("The rating of the game is : {0}", rating);
+            Console.WriteLine("The release date of the game is : {0}", releasedate);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game1;
+            game1.name = "Angry Birds";
+            game1.developer = "Rovio";
+            game1.rating = 9;
+            game1.releasedate = "2010";
+
+            game1.displayInfo();
+            Console.ReadLine();
+        }
+    }
+}

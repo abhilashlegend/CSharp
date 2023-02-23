256256999eecaf12a4a5087c69fec5e05b443922
abhilashlegend <abhilash456a@live.com>
2023-02-23 21:40:31 +0530

    Some of the methods of Math Class

---
+++ MathClass/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MathClass
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Ceiling :" + Math.Ceiling(15.3));
+            Console.WriteLine("Flooring :" + Math.Floor(15.3));
+            Console.WriteLine("3 to the power 5 is " + Math.Pow(3, 5));
+            Console.WriteLine("THe value if pi is " + Math.PI);
+            Console.WriteLine("The square root of 25 is " + Math.Sqrt(25));
+            Console.WriteLine("Always positive is " + Math.Abs(-25));
+            Console.WriteLine("Cos of 1 is :" + Math.Cos(1));
+            Console.ReadLine();
+        }
+    }
+}

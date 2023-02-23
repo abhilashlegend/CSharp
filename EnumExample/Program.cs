aab90f19d0d143ff66d8864f30985c1f36ce1fcd
abhilashlegend <abhilash456a@live.com>
2023-02-23 21:21:50 +0530

    Enum example

---
+++ EnumExample/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EnumExample
+{
+    enum weeks { Mon = 1, Tue, Wed, Thu, Fri, Sat, Sun };
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateOnly  dt = new DateOnly();
+            Console.WriteLine((int)weeks.Mon == dt.Day);
+            DateTime today = DateTime.Now;
+            Console.WriteLine((weeks)(int)today.DayOfWeek);
+
+          
+            Console.ReadLine();
+        }
+    }
+}

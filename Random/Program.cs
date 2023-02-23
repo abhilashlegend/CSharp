9e8efc60169183a9907524bf6338af1f6ceae171
abhilashlegend <abhilash456a@live.com>
2023-02-23 22:03:00 +0530

    Random class example

---
+++ Random/Program.cs
@@ -0,0 +1,15 @@
+﻿Random randomNum = new Random();
+string symbol = "=";
+int num;
+for(int i = 0; i < 10; i++)
+{
+    num = randomNum.Next(0, 150);
+    Console.Write(i + "|");
+    for(int j = 0; j < num; j++)
+    {
+        Console.Write("=");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadLine();

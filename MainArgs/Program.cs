7da505b80278712451746891ce3ca7a80453cf9e
abhilashlegend <abhilash456a@live.com>
2023-02-27 22:11:48 +0530

    Passing args in console app

---
+++ MainArgs/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MainArgs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("No command passed");
+            } 
+            else
+            {
+                if (args[0] == "start")
+                {
+                    for (int i = 0; i <= 10; i++)
+                    {
+                        Console.WriteLine(i);
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                    }
+                    Console.WriteLine("Ending.....");
+                }
+            }
+            
+        }
+    }
+}

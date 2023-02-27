97eacc7dc7d847dc4151fdb2a1d24397819728db
abhilashlegend <abhilash456a@live.com>
2023-02-27 20:10:15 +0530

    Nullable and null collasing

---
+++ Nullable/Program.cs
@@ -0,0 +1,21 @@
+﻿int? age = null;
+if (age != null)
+{
+    global::System.Console.WriteLine("The age is {0}", age);
+}
+else
+{
+    global::System.Console.WriteLine("No age given");
+}
+
+/* Null Collacing example */
+int? a = null;
+
+int b = 4;
+
+int c = a ?? b;
+
+Console.WriteLine("The value of c is {0}", c);
+
+
+Console.ReadLine();

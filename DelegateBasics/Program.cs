142543909d580068d7a2e660f55673b23d6c3afb
abhilashlegend <abhilash456a@live.com>
2023-03-01 20:12:35 +0530

    Basic delegate example using filter

---
+++ DelegateBasics/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace DelegateBasics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> names = new List<string>()
+            {
+                "Abhilash",
+                "Abhishek",
+                "Akhil",
+                "Arjun",
+                "Basavraj",
+                "Chetan",
+                "Manjunath",
+                "Nikhil",
+                "Nagraj",
+                "Nilesh",
+                "Srinivas",
+                "Rajkishor",
+                "Rohit",
+                "Vinay"
+            };
+
+            Console.WriteLine("---------------- Before Filter -----------------------");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            names.RemoveAll(FilterNames);
+
+            Console.WriteLine("------------------ After Filter ------------------------");
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+        }
+
+        public static bool FilterNames(string name)
+        {
+            return name.Contains("i");
+        }
+    }
+}

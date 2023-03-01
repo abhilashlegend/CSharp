8106cd60e4dec0d987936f97f9802bbc84ce1b9e
abhilashlegend <abhilash456a@live.com>
2023-03-01 22:35:59 +0530

    Anonymous functions using delegate

--- DelegateFilter/Program.cs
+++ DelegateFilter/Program.cs
@@ -15,12 +15,26 @@
             Person p3 = new Person() { Name = "Nikhil", Age = 25 };
             Person p4 = new Person() { Name = "Rohan", Age = 17 };
             Person p5 = new Person() { Name = "Vivek", Age = 13 };
+            Person p6 = new Person() { Name = "Surekha", Age = 73 };
 
-            List<Person> people = new List<Person>() { p1, p2, p3, p4, p5 };
+            List<Person> people = new List<Person>() { p1, p2, p3, p4, p5, p6 };
+
+            PersonFilterDelegate filter = delegate (Person p)
+            {
+                return p.Age >= 20 && p.Age < 30;
+            };
 
             DisplayPeople("Minor", people, isMinor);
             DisplayPeople("Adults", people, isAdult);
+            DisplayPeople("People in twenties", people, filter);
+            DisplayPeople("Sceniar", people, delegate (Person p)
+            {
+                return p.Age > 60;
+            });
+
             Console.ReadLine();
+
+           
         }
 
         public static void DisplayPeople(string title,List<Person> people, PersonFilterDelegate filter)

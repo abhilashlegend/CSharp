8a278ab440af512c80a527ddd9048c17a3536775
abhilashlegend <abhilash456a@live.com>
2023-03-01 19:26:27 +0530

    Simple delegate example

---
+++ DelegateExample/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DelegateExample
+{
+    delegate void DelegateSendMessage(string msg);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DelegateSendMessage dsm = new DelegateSendMessage(SendMessage);
+            dsm("Hello from delegate");
+            Console.ReadLine();
+        }
+
+        public static void SendMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}

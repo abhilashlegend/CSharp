06d2e21bc39c050e26c4f569231ffb9b9a386567
abhilashlegend <abhilash456a@live.com>
2023-03-29 20:16:43 +0530

    Adding radion button in WPF

---
+++ WPFRadioImg/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFRadioImg
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            rbMaybe.IsChecked = true;
+        }
+
+        private void NoChecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Please say yes");
+        }
+
+        private void YesChecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Thank you");
+        }
+
+        private void MayBeChecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Please say yes");
+        }
+    }
+}

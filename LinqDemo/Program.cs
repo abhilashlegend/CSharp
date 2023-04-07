cf98683e11e72f1bf303e6bb1d3906840919f117
abhilashlegend <abhilash456a@live.com>
2023-04-07 21:17:12 +0530

    Linq demo

---
+++ LinqDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+oddNumbers(numbers);
+
+evenNumbers(numbers);
+
+Console.ReadLine();
+
+void oddNumbers(int[] numbers) {
+    Console.WriteLine("Odd Numbers: ");
+
+    IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number;
+
+    foreach(int i in oddNumbers)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+void evenNumbers(int[] numbers)
+{
+    Console.WriteLine("Even numbers");
+
+    IEnumerable<int> evenNumbers = from number in numbers where number % 2 == 0 select number;
+
+    foreach(int i in evenNumbers)
+    {
+        Console.WriteLine(i);
+    }
+}
--- ZooManager/MainWindow.xaml.cs
+++ ZooManager/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace ZooManager
 {
@@ -20,9 +24,56 @@ namespace ZooManager
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        SqlConnection sqlConnection;
+        private readonly IConfiguration _config;
+
+        public MainWindow(IConfiguration config)
         {
             InitializeComponent();
+            _config = config;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["Data Source=ABHILASH\\SQLEXPRESS;Initial Catalog=ZooDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+            ShowZoos();
+
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+
+        private void ShowZoos()
+        {
+            try
+            {
+                string query = "select * from Zoo";
+
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    DataTable zooTable = new DataTable();
+
+                    sqlDataAdapter.Fill(zooTable);
+
+                    ListZoos.DisplayMemberPath = "Location";
+
+                    ListZoos.SelectedValuePath = "Id";
+
+                    ListZoos.ItemsSource = zooTable.DefaultView;
+
+
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
